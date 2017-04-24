e8c90a5d676702c723c0df40849fe6d712342cd0
Yask00 <jasstreb@abv.bg>
2017-04-24 13:55:11 +0300

    libraries

--- config/system.config.js
+++ config/system.config.js
@@ -3,7 +3,13 @@ SystemJS.config({
     'map': {
         'plugin-babel': '../node_modules/systemjs-plugin-babel/plugin-babel.js',
         'systemjs-babel-build': '../node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+        
         'jquery': '../node_modules/jquery/dist/jquery.js',
+        //for HTML rendering:
+        'handlebars': '../node_modules/handlebars/dist/handlebars.js',
+        //UI for tabs, autocomplete, calendar otr else
+        'jqueryUi': '../node_modules/jquery-ui-dist/jquery-ui.js'
+
         //'main': './app-modules/main.js',
         //'data': './app-modules/data.js',
         //'utils': './app-modules/utils.js',
